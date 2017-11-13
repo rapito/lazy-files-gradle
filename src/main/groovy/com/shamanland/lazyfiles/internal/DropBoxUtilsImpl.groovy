34033fb1617e47fa589a4b1faa4ae53300885fcb
Robert Peralta <robert.peralta@helloglobo.com>
2017-11-13 11:18:19 -0400

    Use v2

--- src/main/groovy/com/shamanland/lazyfiles/internal/DropBoxUtilsImpl.groovy
+++ src/main/groovy/com/shamanland/lazyfiles/internal/DropBoxUtilsImpl.groovy
@@ -1,22 +1,22 @@
 package com.shamanland.lazyfiles.internal
 
-import com.dropbox.core.v1.DbxClientV1
-import com.dropbox.core.v1.DbxEntry
+import com.dropbox.core.v2.DbxClientV2
+import com.dropbox.core.v2.files.FileMetadata
 import com.shamanland.lazyfiles.LazyFilesItem
 import com.shamanland.lazyfiles.LazyFilesPlugin
 
 class DropBoxUtilsImpl implements DropBoxUtils {
     @Override
     boolean fetch(String accessToken, LazyFilesItem item) {
-        DbxClientV1 client = new DbxClientV1(DropBoxHelper.config, accessToken)
+        DbxClientV2 client = new DbxClientV2(DropBoxHelper.config, accessToken)
         LazyFilesPlugin.logger.info "DropBox account: " + client.getAccountInfo().displayName
 
         FileOutputStream os = new FileOutputStream(item.local)
 
         try {
             LazyFilesPlugin.logger.info "Start fetching: " + item.dropbox
-            DbxEntry.File fetchedFile = client.getFile(item.dropbox, null, os);
-            if (fetchedFile == null) {
+            FileMetadata metadata = client.files().downloadBuilder(item.dropbox).download(os)
+            if (metadata == null) {
                 throw new AssertionError("Couldn't fetch " + item.dropbox)
             }
 
@@ -30,15 +30,17 @@ class DropBoxUtilsImpl implements DropBoxUtils {
 
     @Override
     boolean upload(String accessToken, LazyFilesItem item) {
-        DbxClientV1 client = new DbxClientV1(DropBoxHelper.config, accessToken)
+        DbxClientV2 client = new DbxClientV2(DropBoxHelper.config, accessToken)
         LazyFilesPlugin.logger.info "DropBox account: " + client.getAccountInfo().displayName
 
         FileInputStream is = new FileInputStream(item.local)
 
         try {
             LazyFilesPlugin.logger.info "Start uploading: " + item.local.path
-            DbxEntry.File uploadedFile = client.uploadFile(item.dropbox, DbxWriteMode.add(), item.local.length(), is)
-            if (uploadedFile == null) {
+
+            FileMetadata metadata = client.files().uploadBuilder(item.dropbox)
+                    .uploadAndFinish(is)
+            if (metadata == null) {
                 throw new AssertionError("Couldn't upload " + item.local)
             }
 
